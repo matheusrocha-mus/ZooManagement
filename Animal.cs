679f1fa140d3881f7dadfdbe2e06280d075cdf72
Matheus Caetano Rocha <matheuscaetanorocha@gmail.com>
2023-03-12 20:07:45 -0300

    Correct implementation of constructor method

--- Animal.cs
+++ Animal.cs
@@ -13,205 +13,53 @@ namespace ZooManagement
         private int age;
         private string kingdom;
         private string enclosure;
-        private string alimentation;
+        private string diet;
         private string behaviour;
 
         public string Name
         {
             get { return name; }
-            set { name = value[0].ToString().ToUpper() + value.Substring(1).ToLower(); } // Turns the first letter of the name into upper case and the others into lower case.
+            set { name = value; }
         }
 
         public int Age
         {
             get { return age; }
-            set
-            {
-                if (int.TryParse(value.ToString(), out int validAge) & value > 0)
-                {
-                    age = validAge;
-                }
-                // Used to check if the user input is valid -> a valid SSN has exactly 9 numbers and only nummbers. 
-                // If conversion from String to Int is succesfull, it's value is stored in `validSSN`.
-                else
-                {
-                    age = -1; // If user input is not valid, SSN user input in `CreateAccount` will be asked again until it is valid.
-                }
-            }
+            set { age = value; }
         }
 
         public string Kingdom
         {
             get { return kingdom; }
-            set
-            {
-                switch (value.ToLower())
-                {
-                    case "1":
-                    case "fish":
-                    case "2":
-                    case "amphibian":
-                    case "3":
-                    case "reptile":
-                    case "4":
-                    case "bird":
-                    case "5":
-                    case "mammal":
-                        kingdom = value[0].ToString().ToUpper() + value.Substring(1).ToLower();
-                        break;
-
-                    default:
-                        kingdom = null;
-                        break;
-                }
-            }
+            set { kingdom = value; }
         }
 
         public string Enclosure
         {
             get { return enclosure; }
-            set
-            {
-                switch (value.ToLower())
-                {
-                    case "1":
-                    case "wild":
-                    case "2":
-                    case "captive":
-                        enclosure = value[0].ToString().ToUpper() + value.Substring(1).ToLower();
-                        break;
-
-                    default:
-                        enclosure = null;
-                        break;
-                }
-            }
+            set { enclosure = value; }
         }
 
-        public string Alimentation
+        public string Diet
         {
-            get { return alimentation; }
-            set
-            {
-                switch (value.ToLower())
-                {
-                    case "1":
-                    case "herbivore":
-                    case "2":
-                    case "carnivore":
-                    case "3":
-                    case "omnivore":
-                        alimentation = value[0].ToString().ToUpper() + value.Substring(1).ToLower();
-                        break;
-
-                    default:
-                        alimentation = null;
-                        break;
-                }
-            }
+            get { return diet; }
+            set { diet = value; }
         }
 
         public string Behaviour
         {
             get { return behaviour; }
-            set
-            {
-                switch (value.ToLower())
-                {
-                    case "1":
-                    case "aggressive":
-                    case "2":
-                    case "docile":
-                        behaviour = value[0].ToString().ToUpper() + value.Substring(1).ToLower();
-                        break;
-
-                    default:
-                        behaviour = null;
-                        break;
-                }
-            }
+            set { behaviour = value; }
         }
 
-        public Animal()
+        public Animal(string name, int age, string kingdom, string enclosure, string diet, string behaviour)
         {
-            Console.WriteLine("Enter the animal's name:");
-            Name = Console.ReadLine();
-            Console.Clear();
-
-            Console.WriteLine("Enter the animal's age:");
-            while (Age == -1) // Age will be asked from the user again if entry is not valid.
-            {
-                Age = Convert.ToInt32(Console.ReadLine());
-                if (Age == -1)
-                {
-                    Console.Clear();
-                    Console.WriteLine("Invalid input for 'Age'. A valid age consist only of integer numbers.");
-                    System.Threading.Thread.Sleep(2000);
-                    Console.Clear();
-                    Console.WriteLine("Enter the animal's age:");
-                }
-            }
-            Console.Clear();
-
-            Console.WriteLine("Enter the animal's kingdom:\n\n1) Fish\n2) Amphibian\n3) Reptile\n4) Bird\n5) Mammal");
-            while (Kingdom == null)
-            {
-                Kingdom = Console.ReadLine();
-                if (Kingdom == null)
-                {
-                    Console.Clear();
-                    Console.WriteLine("Invalid input for 'Kingdom'. Please enter a valid option (1, 2, 3, 4 or 5).");
-                    System.Threading.Thread.Sleep(2000);
-                    Console.Clear();
-                    Console.WriteLine("Enter the animal's kingdom:\n\n1) Fish\n2) Amphibian\n3) Reptile\n4) Bird\n5) Mammal");
-                }
-            }
-            Console.Clear();
-
-            Console.WriteLine("Enter the animal's enclosuring:\n\n1) Wild\n2) Captive");
-            while (Enclosure == null)
-            {
-                Enclosure = Console.ReadLine();
-                if (Enclosure == null)
-                {
-                    Console.Clear();
-                    Console.WriteLine("Invalid input for 'Enclosure'. Please enter a valid option (1 or 2).");
-                    System.Threading.Thread.Sleep(2000);
-                    Console.Clear();
-                    Console.WriteLine("Enter the animal's enclosuring:\n\n1) Wild\n2) Captive");
-                }
-            }
-            Console.Clear();
-
-            Console.WriteLine("Enter the animal's alimentation:\n\n1) Herbivore\n2) Carnivore\n3) Omnivore");
-            while (Alimentation == null)
-            {
-                Alimentation = Console.ReadLine();
-                if (Alimentation == null)
-                {
-                    Console.Clear();
-                    Console.WriteLine("Invalid input for 'Alimentation'. Please enter a valid option (1, 2 or 3).");
-                    System.Threading.Thread.Sleep(2000);
-                    Console.Clear();
-                    Console.WriteLine("Enter the animal's alimentation:\n\n1) Herbivore\n2) Carnivore\n3) Omnivore");
-                }
-            }
-            Console.Clear();
-
-            Console.WriteLine("Enter the animal's behaviour:\n\n1) Aggressive\n2) Docile");
-            while (Behaviour == null)
-            {
-                Behaviour = Console.ReadLine();
-                if (Behaviour == null)
-                {
-                    Console.Clear();
-                    Console.WriteLine("Invalid input for 'Behaviour'. Please enter a valid option (1 or 2).");
-                    System.Threading.Thread.Sleep(2000);
-                    Console.Clear();
-                    Console.WriteLine("Enter the animal's behaviour:\n\n1) Aggressive\n2) Docile");
-                }
-            }
-            Console.Clear();
+            Name = name;
+            Age = age;
+            Kingdom = kingdom;
+            Enclosure = enclosure;
+            Diet = diet;
+            Behaviour = behaviour;
         }
 
         public void ShowAnimal()
@@ -220,7 +68,7 @@ namespace ZooManagement
             Console.WriteLine("kingdom: " + Kingdom);
             Console.WriteLine("Age: " + Age);
             Console.WriteLine("Enclosure: " + Enclosure);
-            Console.WriteLine("Alimentation: " + Alimentation);
+            Console.WriteLine("Diet: " + Diet);
             Console.WriteLine("Behaviour: " + Behaviour);
         }
     }
--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Reflection;
 
 namespace ZooManagement
 {
@@ -32,37 +34,156 @@ namespace ZooManagement
                     case "yes":
                     case "1":
                         keepRegistering = true;
-                        Animal newAnimal = new Animal();
+
+                        string name = null;
+                        while (name == null)
+                        {
+                            Console.WriteLine("Enter the animal's name:");
+                            input = Console.ReadLine();
+                            if (!string.IsNullOrEmpty(input) & Regex.IsMatch(input, @"^[a-zA-Z\s]+$"))
+                            {
+                                string[] names = input.Split(' ');
+                                for (int i = 0; i < names.Length; i++)
+                                {
+                                    names[i] = names[i][0].ToString().ToUpper() + names[i].Substring(1).ToLower();
+                                }
+                                name = string.Join(" ", names);
+                            }
+                            else
+                            {
+                                name = null;
+                                Console.Clear();
+                                Console.WriteLine("Invalid input. A valid name has no numbers or special characters.");
+                                System.Threading.Thread.Sleep(2000);
+                                Console.Clear();
+                            }
+                        }
+
+                        int age = -1;
+                        while (age == -1)
+                        {
+                            Console.WriteLine("Enter the animal's age (years):");
+                            input = Console.ReadLine();
+                            if (!string.IsNullOrEmpty(input) & int.TryParse(input, out int parsedAge) & parsedAge >= 0)
+                            {
+                                age = parsedAge;
+                            }
+                            else
+                            {
+                                age = -1;
+                                Console.Clear();
+                                Console.WriteLine("Invalid input. A valid age consists only of integer numbers (animal's years old).");
+                                System.Threading.Thread.Sleep(2000);
+                                Console.Clear();
+                            }
+                        }
+
+                        string kingdom = null;
+                        while (kingdom == null)
+                        {
+                            Console.WriteLine("Enter the animal's kingdom:\n\n1) Fish\n2) Amphibian\n3) Reptile\n4) Bird\n5) Mammal ");
+                            input = Console.ReadLine();
+                            switch (input.ToLower())
+                            {
+                                case "1":
+                                case "fish":
+                                case "2":
+                                case "amphibian":
+                                case "3":
+                                case "reptile":
+                                case "4":
+                                case "bird":
+                                case "5":
+                                case "mammal":
+                                    kingdom = input[0].ToString().ToUpper() + input.Substring(1).ToLower();
+                                    break;
+
+                                default:
+                                    kingdom = null;
+                                    Console.Clear();
+                                    Console.WriteLine("Invalid input. A valid kingdom would be one of the presented options (1, 2, 3, 4 or 5).");
+                                    System.Threading.Thread.Sleep(2500);
+                                    Console.Clear();
+                                    break;
+                            }
+                        }
+
+                        string enclosure = null;
+                        while (enclosure == null)
+                        {
+                            Console.WriteLine("Enter the animal's enclosuring type:\n\n1) Wild\n2) Captive");
+                            input = Console.ReadLine();
+                            switch (input.ToLower())
+                            {
+                                case "1":
+                                case "wild":
+                                case "2":
+                                case "captive":
+                                    enclosure = input[0].ToString().ToUpper() + input.Substring(1).ToLower();
+                                    break;
+
+                                default:
+                                    enclosure = null;
+                                    Console.Clear();
+                                    Console.WriteLine("Invalid input. A valid enclosure would be one of the presented options (1 or 2).");
+                                    System.Threading.Thread.Sleep(2500);
+                                    Console.Clear();
+                                    break;
+                            }
+                        }
+
+                        string diet = null;
+                        while (diet == null)
+                        {
+                            Console.WriteLine("Enter the animal's diet:\n\n1) Herbivore\n2) Carnivore\n3) Omnivore");
+                            input = Console.ReadLine();
+                            switch (input.ToLower())
+                            {
+                                case "1":
+                                case "herbivore":
+                                case "2":
+                                case "carnivore":
+                                case "3":
+                                case "omnivore":
+                                    diet = input[0].ToString().ToUpper() + input.Substring(1).ToLower();
+                                    break;
+
+                                default:
+                                    diet = null;
+                                    Console.Clear();
+                                    Console.WriteLine("Invalid input. A valid diet would be one of the presented options (1, 2 or 3).");
+                                    System.Threading.Thread.Sleep(2500);
+                                    Console.Clear();
+                                    break;
+                            }
+                        }
+
+                        string behaviour = null;
+                        while (behaviour == null)
+                        {
+                            Console.WriteLine("Enter the animal's behaviour type:\n\n1) Aggressive\n2) Docile");
+                            input = Console.ReadLine();
+                            switch (input.ToLower())
+                            {
+                                case "1":
+                                case "aggressive":
+                                case "2":
+                                case "docile":
+                                    behaviour = input[0].ToString().ToUpper() + input.Substring(1).ToLower();
+                                    break;
+
+                                default:
+                                    behaviour = null;
+                                    Console.Clear();
+                                    Console.WriteLine("Invalid input. A valid behaviour would be one of the presented options (1 or 2).");
+                                    System.Threading.Thread.Sleep(2500);
+                                    Console.Clear();
+                                    break;
+                            }
+                        }
+
+                        Animal newAnimal = new Animal(name, age, kingdom, enclosure, diet, behaviour);
                         animalList.Add(newAnimal);
-                        switch (newAnimal.Kingdom)
-                        {
-                            case "Fish":
-
-                                break;
-                            case "Amphibian":
-                                break;
-                            case "Reptile":
-                                break;
-                            case "Bird":
-                                break;
-                            case "Mammal":
-                                break;
-                        }
-
-                        switch (newAnimal.Enclosure)
-                        {
-
-                        }
-
-                        switch (newAnimal.Alimentation)
-                        {
-
-                        }
-
-                        switch (newAnimal.Behaviour)
-                        {
-
-                        }
                         break;
 
                     case "no":
